45297a11f0e94c3a87f2fa4cf00d956be3c89e95
chaosdomok <31513498+chaosdomok@users.noreply.github.com>
2024-03-02 18:33:54 +0100

    Add files via upload

---
+++ CarWorkshop.Infastructure/Migrations/20240201183103_CarWorkshopAbout.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWorkshop.Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarWorkshopAbout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactDetails_Post",
+                table: "CarWorkshops",
+                newName: "ContactDetails_PostalCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "CarWorkshops",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "CarWorkshops");
+
+            migrationBuilder.RenameColumn(
+                name: "ContactDetails_PostalCode",
+                table: "CarWorkshops",
+                newName: "ContactDetails_Post");
+        }
+    }
+}
---
+++ CarWorkshop.Infastructure/Repositories/CarWorkshopRepository.cs
@@ -0,0 +1,51 @@
+﻿using CarWorkshop.Domain.Interfaces;
+using CarWorkshop.Domain.Entities;
+using CarWorkshop.Infastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infastructure.Repositories
+{
+    internal class CarWorkshopRepository : ICarWorkshopRepository
+    {
+        private CarWorkshopDbContext _dbContext;
+
+        public CarWorkshopRepository(CarWorkshopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Commit()
+            => _dbContext.SaveChangesAsync();
+        public async Task Create(Domain.Entities.CarWorkshop carWorkshop) 
+        {
+            _dbContext.Add(carWorkshop);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Domain.Entities.CarWorkshop>> GetAll()
+        {
+            try
+            {
+                var carWorkshops = await _dbContext.CarWorkshops.ToListAsync();
+                return carWorkshops;
+            }
+            catch (Exception ex)
+            {
+                // Obsługa błędów - możesz zalogować błąd lub podjąć inne działania w przypadku wystąpienia wyjątku
+                Console.WriteLine($"Wystąpił błąd podczas pobierania warsztatów samochodowych: {ex.Message}");
+                return Enumerable.Empty<Domain.Entities.CarWorkshop>(); // Zwróć pustą kolekcję w przypadku błędu
+            }
+        }
+
+        public async Task<Domain.Entities.CarWorkshop> GetByEncodedName(string encodedName)
+            => await _dbContext.CarWorkshops.FirstAsync(c => c.EncodedName == encodedName);
+
+        public Task<Domain.Entities.CarWorkshop?> GetByName(string name) 
+            => _dbContext.CarWorkshops.FirstOrDefaultAsync(cw => cw.Name.ToLower() == name.ToLower()); 
+    }
+}
